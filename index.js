359cb7b0566f08d116e3347c81453e0530a16788
apurbochaki-lab <apurbokumarchaki@gmail.com>
2026-05-20 18:43:10 +0600

    PATCH route added for Manage Facilitites to update the data of database

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-// 
-// 
 
 const uri = process.env.MONGO_URI;
 
@@ -27,52 +25,67 @@ const client = new MongoClient(uri, {
 
 async function server() {
     try {
-        await client.connect();
-        
+        // await client.connect();
+
         const db = client.db("SportNest-DB");
         const sportsCollection = db.collection("sports")
         const bookingCollection = db.collection("bookings")
 
         // All Sports Routes
-        app.get('/sports', async(req, res) => {
+        app.get('/sports', async (req, res) => {
             const result = await sportsCollection.find().toArray();
             res.send(result)
         })
 
-        app.get('/sports/:id', async(req, res) => {
+        app.get('/sports/:id', async (req, res) => {
             const id = req.params.id;
-            const result = await sportsCollection.findOne({_id: new ObjectId(id)});
+            const result = await sportsCollection.findOne({ _id: new ObjectId(id) });
             res.send(result)
         })
 
-        app.get('/featured', async(req, res) => {
+        app.get('/featured', async (req, res) => {
             const result = await sportsCollection.find().skip(4).limit(6).toArray();
             res.send(result)
         })
 
 
         // Post Routes
-        app.post('/sports', async(req, res) => {
+        app.post('/sports', async (req, res) => {
             const formData = req.body;
             // console.log("Data from client : ", formData)
             const result = await sportsCollection.insertOne(formData);
             res.send(result);
         })
         // Bookings
-        app.post('/bookings', async(req, res) => {
+        app.post('/bookings', async (req, res) => {
             const bookingData = req.body;
             console.log(bookingData)
             const result = await bookingCollection.insertOne(bookingData);
             res.send(result)
         })
 
-        app.get('/bookings', async(req, res) => {
+        app.get('/bookings', async (req, res) => {
             const result = await bookingCollection.find().toArray();
             res.send(result)
         })
 
+        // Manage My Facilities PATCH
+        app.patch('/sports', async (req, res) => {
+            const id = req.headers.id;
+            const updatedData = req.body;
+            console.log(id)
+            console.log(updatedData)
 
-        await client.db("admin").command({ ping: 1 });
+            const result = await sportsCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: updatedData }
+            )
+
+            res.send(result)
+        })
+
+
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
